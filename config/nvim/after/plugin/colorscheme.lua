f56345f35ac9650d62f41e5576cfa466c4f56871
Jace Babin <jbabin91@gmail.com>
2021-11-04 13:32:32 -0700

    Removed nvim configs

--- config/nvim/after/plugin/colorscheme.lua
+++
@@ -1,5 +0,0 @@
--- colorscheme configs
-vim.opt.termguicolors = true
-vim.g.tokyonight_style = "night"
-vim.cmd("colorscheme tokyonight")
-require("colorizer").setup()
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,1 @@
-require("cfg.disabled")
-local status_ok, _ = pcall(require, "impatient")
-if status_ok then
-	require("impatient")
-end
-require("cfg.bootstrap") -- plugins configs
-require("cfg.globals") -- global configs
-require("cfg.editor") -- edit configs
+
